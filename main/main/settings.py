1d9a55b16a3f71225827df200db5e09e0be96d31
msg8925 <msg.8925@gmail.com>
2022-11-04 16:14:18 +0700

    Login route added

--- main/main/settings.py
+++ main/main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set the page where the user will be redirected once successfully logged in
+LOGIN_REDIRECT='blog-home'
+
+# Set the URL where the login page is located
+LOGIN_URL='login'

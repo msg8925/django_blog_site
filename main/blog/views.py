03500c26b6cd57cf742d428e1fce39304ebb7ddf
msg8925 <msg.8925@gmail.com>
2022-11-04 15:00:55 +0700

    Post content added to home page

--- main/blog/views.py
+++ main/blog/views.py
@@ -4,10 +4,10 @@ from .models import Post
 # function-based views here.
 def home(request):
 
-    # posts = Post.objects.get()
+    posts = Post.objects.all()
 
-    # context = {
-    #     'posts': posts
-    # }
+    context = {
+        'posts': posts
+    }
 
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)

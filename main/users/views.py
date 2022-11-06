35d65ebf57d0bf9a7e91d1dca921168e77e7aea1
msg8925 <msg.8925@gmail.com>
2022-11-06 14:07:29 +0700

    Registration page in progress

--- main/users/views.py
+++ main/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from users.forms import UserRegistrationForm
 
 # Create your views here.
+
+def register(request):
+
+    # If the user submits a form
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.form)
+
+
+    form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/register.html', context)
+

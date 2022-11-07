710ba518a0753b2421d81e3bae638a99b65a0e43
msg8925 <msg.8925@gmail.com>
2022-11-07 11:58:12 +0700

    Migrations added for Post model

--- main/blog/models.py
+++ main/blog/models.py
@@ -5,7 +5,7 @@ import datetime
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField(max_length=250)
+    content = models.TextField()
     date_posted = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 

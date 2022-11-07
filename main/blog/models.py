b8f1c4672098845ec8f1b64692619914d9130078
msg8925 <msg.8925@gmail.com>
2022-11-07 11:36:36 +0700

    Post create view added

--- main/blog/models.py
+++ main/blog/models.py
@@ -8,3 +8,4 @@ class Post(models.Model):
     content = models.CharField(max_length=250)
     date_posted = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- main/blog/views.py
+++ main/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post 
+from django.views.generic import CreateView
 
 # function-based views here.
 def home(request):
@@ -11,3 +12,15 @@ def home(request):
     }
 
     return render(request, 'blog/home.html', context)
+
+
+#def post(request):
+
+#    return render(request, 'blog/post-create.html')
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post-create.html'
+    fields = ['title', 'content']
+    

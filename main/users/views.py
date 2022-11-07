a1fb6c82d7d9df1556f33cc86421ea91ce104901
msg8925 <msg.8925@gmail.com>
2022-11-07 13:58:57 +0700

    Profile page completed

--- main/users/views.py
+++ main/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -24,5 +25,27 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+@login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+
+    if request.method == 'POST':
+        form_u = UserUpdateForm(request.POST, instance=request.user)
+        form_p = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form_u.is_valid() & form_p.is_valid():
+            form_u.save()
+            form_p.save()
+            messages.success(request, 'Profile Updated') 
+            return redirect('profile')
+
+
+    else:
+        form_u = UserUpdateForm(instance=request.user)
+        form_p = ProfileUpdateForm(instance=request.user.profile)
+
+
+    context = {
+        'form_u': form_u,
+        'form_p': form_p
+    }
+
+    return render(request, 'users/profile.html', context)
